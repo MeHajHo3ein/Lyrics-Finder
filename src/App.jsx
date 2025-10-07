319772641a35ea6471a193dc1d83a8b75bd4e6c9
Ho3ein <mehajho3ein@gmail.com>
2025-10-07 20:52:12 +0330

    style: enhance UI TailwindCSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -30,15 +30,29 @@ function App() {
   }
 
   return (
-    <div>
-      <h1>Lyrics Finder</h1>
-      <input type="text" placeholder="Artist name..." onChange={(e) => setArtist(e.target.value)} />
-      <input type="text" placeholder="Song name..." onChange={(e) => setSong(e.target.value)} />
-      <button onClick={() => searchLyrics()}>Search</button>
-
+    <div className="min-h-[100vh] h-auto text-center sm:text-center">
+      <h1 className="text-[#ff7a8a] text-4xl font-bold mt-10 mb-5">Lyrics Finder</h1>
+      <input
+        className="w-50 border-2 focus:outline-[#d46976] rounded-sm mr-2 p-1 "
+        type="text"
+        placeholder="Artist name..."
+        onChange={(e) => setArtist(e.target.value)}
+      />
+      <input
+        className="w-50 border-2 focus:outline-[#d46976] rounded-sm mr-2 p-1"
+        type="text"
+        placeholder="Song name..."
+        onChange={(e) => setSong(e.target.value)}
+      />
+      <button
+        className="w-20 cursor-pointer bg-[#d46976] text-amber-100 p-1.5 rounded-sm outline-0 "
+        onClick={() => searchLyrics()}
+      >
+        Search
+      </button>
+      <hr className="mt-5 w-3/6 m-auto" />
       {loading && <p>Loading the lyrics...</p>}
-
-      <pre>{lyrics}</pre>
+      <pre className="text-lg mt-3 text-[#686868]">{lyrics}</pre>
     </div>
   );
 }
